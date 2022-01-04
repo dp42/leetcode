68aabcb0ce9c21c87bf0c437017361e78d6c81a8
Koichi SATO <dp42@users.noreply.github.com>
2022-01-05 00:29:20 +0900

    Expand Around Center

--- ruby/5_longest_palindrome.rb
+++ ruby/5_longest_palindrome.rb
@@ -6,50 +6,35 @@ def longest_palindrome(s)
   return s if s == s.reverse
   return s if s.length < 2
 
-  chars = {}
-  last_idx = s.length - 1
+  slen = s.length
   start = 0
-  maxlen = 1
+  plen = 1
 
-  searched = Array.new(s.length) { Array.new(s.length, false) }
+  i = 0
+  while i < slen
+    len1 = expand_around_center(i, i, s, slen)
+    len2 = expand_around_center(i, i + 1, s, slen)
+    len = len1 > len2 ? len1 : len2
 
-  i = last_idx
-  while i >= 0
-    chars[i] = s[i]
-    searched[i][i] = true
-    searched[i][i + 1] = (s[i] == s[i + 1])
-
-    if s[i] == s[i + 1]
-      searched[i][i + 1] = true
-      start = i
-      maxlen = 2
+    if len > plen
+      plen = len
+      start = i - (plen - 1) / 2
     end
-    i -= 1
+    i += 1
+    # p len2 if len2 > 5
   end
+  s[start, plen]
+end
 
-  i = last_idx
-  while i >= 0
-    j = last_idx
-    while j >= 0
-      break if i > j
+def expand_around_center(l, r, s, slen)
+  left = l
+  right = r
 
-      if i == j || j - i == 1
-        j -= 1
-        next
-      end
-
-      searched[i][j] = (chars[i] == chars[j] && searched[i + 1][j - 1])
-      if searched[i][j] && j - i + 1 > maxlen
-        maxlen = j - i + 1
-        start = i
-      end
-      j -= 1
-    end
-
-    i -= 1
+  while left >= 0 && right < slen && s[left] == s[right]
+    left -= 1
+    right += 1
   end
-
-  s[start, maxlen]
+  right - left - 1
 end
 
 str = "ukxidnpsdfwieixhjnannbmtppviyppjgbsludrzdleeiydzawnfmiiztsjqqqnthwinsqnrhfjxtklvbozkaeetmblqbxbugxycrlzizthtuwxlmgfjokhqjyukrftvfwikxlptydybmmzdhworzlaeztwsjyqnshggxdsjrzazphugckgykzhqkdrleaueuajjdpgagwtueoyfdafacvadfafafdafadfafdccccccferererereedfacccccfdjkafjalkdfjeifjkfjdaklfjdakslfjakldfjieybzanrvrgevolwssvqimgzpkxehnunycmlnetfaflhusauopyizbcpntywntadciopspgyhkelxgwplvncmvncmvncdjjdjfdhjfhdjfhdjfdizyszcwdixzgxzgxiawstbnpjezxinyowmqsysazgwxpthloegxvezsxcvorzquzdtfcvckjpewowazuaynfpxsxrihsfswrmuvluwbdazmcealapulnahgdxxycizeqelesvshkgpavihywwlhdfopmmbwegibxhluantulnccqieyrbjjqtlgkpfezpxmlwpyohdyftzgbeoioquxpnrwrgzlhtlgyfwxtqcgkzcuuwagmlvgiwrhnredtulxudrmepbunyamssrfwyvgabbcfzzjayccvvwxzbfgeglqmuogqmhkjebehtwnmxotjwjszvrvpfpafwomlyqsgnysydfdlbbltlwugtapwgfnsiqxcnmdlrxoodkhaaaiioqglgeyuxqefdxbqbgbltrxcnihfwnzevvtkkvtejtecqyhqwjnnwfrzptzhdnmvsjnnsnixovnotugpzuymkjplctzqbfkdbeinvtgdpcbvzrmxdqthgorpaimpsaenmnyuyoqjqqrtcwiejutafyqmfauufwripmpcoknzyphratopyuadgsfrsrqkfwkdlvuzyepsiolpxkbijqw"
